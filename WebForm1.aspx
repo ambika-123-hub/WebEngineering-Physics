f5af43f3f23e2cdc2ee430d657dd9a4fc4c00a35
ambika-123-hub <97598156+ambika-123-hub@users.noreply.github.com>
2024-05-21 20:29:03 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebEngineeringPhysics.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <table width="1200">
+        <tr>
+            <td width="300">
+                &nbsp;</td>
+            <td align="center" width="600">
+                <asp:Label ID="Label1" runat="server" Text="Conversion Calculator" 
+                    Width="584px"></asp:Label>
+            </td>
+            <td width="300">
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                <table width="600">
+                    <tr>
+                        <td align="center" width="125">
+                            <asp:Button ID="Btn_Length" runat="server" onclick="Btn_Length_Click" 
+                                Text="Length" Width="125px" />
+                        </td>
+                        <td align="center" width="125">
+                            <asp:Button ID="Btn_Temp" runat="server" onclick="Btn_Temp_Click" 
+                                Text="Temprature" Width="125px" />
+                        </td>
+                        <td align="center" width="125">
+                            <asp:Button ID="Btn_Wt" runat="server" Text="Weight" Width="125px" 
+                                onclick="Btn_Wt_Click" />
+                        </td>
+                        <td align="center" width="125">
+                            <asp:Button ID="Btn_time" runat="server" Text="Time" Width="125px" 
+                                onclick="Btn_time_Click" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2">
+                            <asp:Label ID="Label2" runat="server" Text="From"></asp:Label>
+                        </td>
+                        <td align="center" colspan="2">
+                            <asp:Label ID="Label3" runat="server" Text="To"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2">
+                            <asp:TextBox ID="TextBox1" runat="server" Width="250px"></asp:TextBox>
+                        </td>
+                        <td align="center" colspan="2">
+                            <asp:TextBox ID="TextBox2" runat="server" Width="250px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2">
+                            <asp:ListBox ID="ListBox1" runat="server" Width="250px">
+                            </asp:ListBox>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                        </td>
+                        <td align="center" colspan="2" valign="top">
+                            <asp:ListBox ID="ListBox2" runat="server" Width="250px">
+                            </asp:ListBox>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace WebEngineeringPhysics
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_Length_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear(); 
+            ListBox1.Items.Add(new ListItem("meter", "1"));
+            ListBox1.Items.Add(new ListItem("foot", "2"));
+            ListBox1.Items.Add(new ListItem("yard", "3"));
+            ListBox1.Items.Add(new ListItem("inch", "4"));
+            ListBox1.Items.Add(new ListItem("kilometer", "5"));
+            ListBox1.Items.Add(new ListItem("mile", "6"));
+            ListBox1.Items.Add(new ListItem("centimeter", "7"));
+            ListBox1.Items.Add(new ListItem("millimeter", "8"));
+            ListBox1.Items.Add(new ListItem("micrometer", "9"));
+            ListBox1.Items.Add(new ListItem("nanometer", "10"));
+            ListBox1.Items.Add(new ListItem("light year", "11"));
+            ListBox2.Items.Clear();
+            ListBox2.Items.Add(new ListItem("meter", "1"));
+            ListBox2.Items.Add(new ListItem("foot", "2"));
+            ListBox2.Items.Add(new ListItem("yard", "3"));
+            ListBox2.Items.Add(new ListItem("inch", "4"));
+            ListBox2.Items.Add(new ListItem("kilometer", "5"));
+            ListBox2.Items.Add(new ListItem("mile", "6"));
+            ListBox2.Items.Add(new ListItem("centimeter", "7"));
+            ListBox2.Items.Add(new ListItem("millimeter", "8"));
+            ListBox2.Items.Add(new ListItem("micrometer", "9"));
+            ListBox2.Items.Add(new ListItem("nanometer", "10"));
+            ListBox2.Items.Add(new ListItem("light year", "11"));
+
+
+        }
+
+        protected void Btn_Temp_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();        
+            ListBox1.Items.Add(new ListItem("kelvin", "1"));         
+            ListBox1.Items.Add(new ListItem("Celsius", "2"));
+            ListBox1.Items.Add(new ListItem("Farenheit", "3"));
+            ListBox2.Items.Clear();
+            ListBox2.Items.Add(new ListItem("kelvin", "1"));
+            ListBox2.Items.Add(new ListItem("Celsius", "2"));
+            ListBox2.Items.Add(new ListItem("Farenheit", "3"));
+     
+        }
+
+        protected void Btn_Wt_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add(new ListItem("kilogram", "1"));
+            ListBox1.Items.Add(new ListItem("gram", "2"));
+            ListBox1.Items.Add(new ListItem("milligram", "3"));
+            ListBox1.Items.Add(new ListItem("ton", "4"));
+            ListBox1.Items.Add(new ListItem("pound", "5"));
+            ListBox1.Items.Add(new ListItem("ounce", "6"));
+            ListBox1.Items.Add(new ListItem("carrat", "7"));
+            ListBox1.Items.Add(new ListItem("atomic mass unit", "8"));
+            ListBox2.Items.Clear();
+            ListBox2.Items.Add(new ListItem("kilogram", "1"));
+            ListBox2.Items.Add(new ListItem("gram", "2"));
+            ListBox2.Items.Add(new ListItem("milligram", "3"));
+            ListBox2.Items.Add(new ListItem("ton", "4"));
+            ListBox2.Items.Add(new ListItem("pound", "5"));
+            ListBox2.Items.Add(new ListItem("ounce", "6"));
+            ListBox2.Items.Add(new ListItem("carrat", "7"));
+            ListBox2.Items.Add(new ListItem("atomic mass unit", "8"));
+        }
+
+        protected void Btn_time_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add(new ListItem("second", "1"));
+            ListBox1.Items.Add(new ListItem("millisecond", "2"));
+            ListBox1.Items.Add(new ListItem("picosecond", "3"));
+            ListBox1.Items.Add(new ListItem("nanosecond", "4"));
+            ListBox1.Items.Add(new ListItem("minute", "5"));
+            ListBox1.Items.Add(new ListItem("hour", "6"));
+            ListBox1.Items.Add(new ListItem("day", "7"));
+            ListBox1.Items.Add(new ListItem("week", "8"));
+            ListBox1.Items.Add(new ListItem("month", "9"));
+            ListBox1.Items.Add(new ListItem("year", "10"));
+            ListBox2.Items.Clear();
+            ListBox2.Items.Add(new ListItem("second", "1"));
+            ListBox2.Items.Add(new ListItem("millisecond", "2"));
+            ListBox2.Items.Add(new ListItem("picosecond", "3"));
+            ListBox2.Items.Add(new ListItem("nanosecond", "4"));
+            ListBox2.Items.Add(new ListItem("minute", "5"));
+            ListBox2.Items.Add(new ListItem("hour", "6"));
+            ListBox2.Items.Add(new ListItem("day", "7"));
+            ListBox2.Items.Add(new ListItem("week", "8"));
+            ListBox2.Items.Add(new ListItem("month", "9"));
+            ListBox2.Items.Add(new ListItem("year", "10"));
+        }
+
+    }
+}
